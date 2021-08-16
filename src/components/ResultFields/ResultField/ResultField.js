36ec5a7e16851fcf4605a3d32c8bc1ed1e021d51
sanam <simian114@gmail.com>
2021-08-16 21:11:07 +0900

    feat: ResultField(s) 컴포넌트 작성

--- src/components/ResultFields/ResultField/ResultField.js
+++ src/components/ResultFields/ResultField/ResultField.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-export default function ResultField({ value }) {
+export default function ResultField({ value, color, loading }) {
+  if (loading) {
+    <StyledResultField color={color}>loading..</StyledResultField>;
+  }
   return (
-    <div>
-      {value?.map(item => (
-        <div>{item}</div>
-      ))}
-    </div>
+    <StyledResultField color={color}>
+      {value ? value.join(', ') : ''}
+    </StyledResultField>
   );
 }
+
+const StyledResultField = styled.p(({ color }) => ({
+  width: '100%',
+  margin: 'auto',
+  fontSize: 20,
+  // height: 30,
+  backgroundColor: color || '#e1bee7',
+}));
